4026c287e247993375a6f6ae2a1167b6f30d9efc
AhmedElassuty <ahmed.elassuty214@gmail.com>
2016-04-02 01:30:07 +0200

    Add invitation model and migration

---
+++ app/Models/Invitation.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Invitation extends Model
+{
+    /*
+        Mass Assignment
+    */
+
+    protected $fillable = ['inviter_id', 'invited_id', 'invitable_id', 'invitable_type']
+
+    /*
+        Validations
+    */
+
+    $rules = [
+        'inviter_id' => 'required',
+        'invited_id' => 'required',
+        'invitable_type' => 'required',
+        'invitable_id' => 'required'
+    ]
+
+    /*
+     *   Relations
+    */
+
+    // Morph type map
+    Relation::morphMap([
+        App\Models\Ticket::class,
+    ]);
+
+    // Get the invitation owner
+    public function inviter()
+    {
+        return $this->belongsTo('App\Models\User', 'inviter_id');
+    }
+
+    // Get the invited user
+    public function invited()
+    {
+        return $this->belongsTo('App\Models\User', 'invited_id');
+    }
+
+    // Get the object the invited user invited to
+    public function invitable()
+    {
+        return $this->morphTo();
+    }
+}
---
+++ database/migrations/2016_04_01_171003_create_invitation_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateInvitationTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('invitations', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+
+            $table->integer('inviter_id')->unsigned();
+            $table->integer('invited_id')->unsigned();
+            $table->integer('invitable_id')->unsigned();
+            $table->string('invitable_type');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('invitations');
+    }
+}
