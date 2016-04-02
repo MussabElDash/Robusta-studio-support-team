8d89a4d1718deda1442de4c73d514cd79ac5c114
mahmoudawadeen <mahmoud.awadeen@gmail.com>
2016-04-02 19:57:34 +0200

    department model and migration.

--- app/Models/Department.php
+++ app/Models/Department.php
@@ -6,5 +6,33 @@ use Illuminate\Database\Eloquent\Model;
 
 class Department extends Model
 {
-    //
+    /**
+     * The rules used for validation
+     *
+     * @var array
+     */
+    protected $rules = [
+        'name' => 'required'
+
+    ];
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'user_id','description'
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+
+    ];
+    public function head(){
+        return belongs_to('App\Models\User','user_id');
+    }
 }
