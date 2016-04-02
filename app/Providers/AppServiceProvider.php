40a6ff7837c9b919a7996f684c536172ced7cd8c
AhmedElassuty <ahmed.elassuty214@gmail.com>
2016-04-02 03:28:50 +0200

    Remove unneeded as keyword

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,7 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Database\Eloquent\Relations\Relation as Relation;
+use Illuminate\Database\Eloquent\Relations\Relation;
 
 class AppServiceProvider extends ServiceProvider
 {
