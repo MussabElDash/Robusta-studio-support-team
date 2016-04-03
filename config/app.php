780eaa617c805c77a2165b80bd624460ace59013
Mussab ElDash <mussab14@gmail.com>
2016-04-04 01:37:28 +0200

    add the library the handles the slugs and an alias for Input to use in the controllers, in the app.php

--- config/app.php
+++ config/app.php
@@ -157,6 +157,8 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        'Cviebrock\EloquentSluggable\SluggableServiceProvider',
+
     ],
 
     /*
@@ -205,6 +207,8 @@ return [
 
         'Form' => Collective\Html\FormFacade::class,
         'Html' => Collective\Html\HtmlFacade::class,
+
+        'Input'     => Illuminate\Support\Facades\Input::class,
     ],
 
 ];
