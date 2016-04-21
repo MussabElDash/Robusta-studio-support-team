81e5b7757a59b96a2482629230b5c2cd4a6ae3a5
AhmedElassuty <ahmed.elassuty214@gmail.com>
2016-04-22 01:32:04 +0200

    Add ticket and label_ticket migrations

--- app/Models/Ticket.php
+++ app/Models/Ticket.php
@@ -5,5 +5,16 @@ namespace App\Models;
 
 class Ticket extends BaseModel
 {
-    //
+    // Mass Assignment
+    protected $fillable = []
+
+    // Validations
+    protected static $rules = [
+
+    ]
+
+    // Relations
+
+
+    // Methods
 }
