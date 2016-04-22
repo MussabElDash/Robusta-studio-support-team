d89f139724fa5c8d708475417bbcf230f1707244
Mussab ElDash <mussab14@gmail.com>
2016-04-22 19:52:43 +0200

    change the url of the user photo to be absolute path

--- resources/views/layout-components/sidebar/sidebar.blade.php
+++ resources/views/layout-components/sidebar/sidebar.blade.php
@@ -2,7 +2,7 @@
   <section class="sidebar">
     <div class="user-panel">
       <div class="pull-left image">
-        <img src="assets/images/user2-160x160.jpg" class="img-circle" alt="User Image">
+        <img src="/assets/images/user2-160x160.jpg" class="img-circle" alt="User Image">
       </div>
       <div class="pull-left info">
         <p>{{$user->name}}</p>
--- resources/views/layouts/landing.blade.php
+++ resources/views/layouts/landing.blade.php
@@ -46,6 +46,7 @@
                       </a>
 
                       <ul class="dropdown-menu" role="menu">
+                          <li><a href="{{ url('/home') }}"><i class="fa fa-btn fa-sign-out"></i>Home</a></li>
                           <li><a href="{{ url('/logout') }}"><i class="fa fa-btn fa-sign-out"></i>Logout</a></li>
                       </ul>
                   </li>
