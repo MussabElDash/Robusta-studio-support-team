a6cb66e90d47f45c264b800fe397be9220cd2ae7
ahmedhagii <a.akram93@gmail.com>
2016-04-25 17:19:33 +0200

    modifying the csrf tokens to support ajax

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -3,6 +3,10 @@
 namespace App\Http\Middleware;
 
 use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken as BaseVerifier;
+use Illuminate\Support\Facades\Input;
+use Log;
+use Session;
+use Request;
 
 class VerifyCsrfToken extends BaseVerifier
 {
@@ -14,4 +18,34 @@ class VerifyCsrfToken extends BaseVerifier
     protected $except = [
         //
     ];
+
+    /**
+     * Determine if the session and input CSRF tokens match.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @return bool
+     */
+    protected function tokensMatch($request)
+    {
+        // If request is an ajax request, then check to see if token matches token provider in
+        // the header. This way, we can use CSRF protection in ajax requests also.
+
+        if (Request::ajax())
+        {
+            Log::info("tokens ... \n".'   '.Request::header('csrftoken').'   '.Session::token());
+            if (Session::token() !== Request::header('csrftoken'))
+            {
+                // Change this to return something your JavaScript can read...
+                throw new Illuminate\Session\TokenMismatchException;
+            }
+        }
+        elseif (Session::token() !== Input::get('_token'))
+        {
+            Log::info("tokens ... \n".'   '.Input::get('_token').'   '.Session::token());
+            throw new Illuminate\Session\TokenMismatchException;
+        }
+        // $token = $request->ajax() ? $request->header('X-CSRF-Token') : $request->input('_token');
+        //
+        // reeeeturn $request->session()->token() == $token;
+    }
 }
