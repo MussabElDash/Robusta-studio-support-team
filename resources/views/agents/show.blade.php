80cfbb04cea5642b6c7e8879b79eaab1f9b1ba58
Mussab ElDash <mussab14@gmail.com>
2016-04-28 13:57:25 +0200

    styling changes

--- resources/views/agents/show.blade.php
+++ resources/views/agents/show.blade.php
@@ -1,5 +1,49 @@
 @extends('layouts.home')
 
+@section('form_content')
+    <div class="form-group">
+        <label class="col-md-4 control-label">Name</label>
+        <div class="col-md-6">
+            {{ Form::label('name', $agent->name, ['class' => 'form-control']) }}
+        </div>
+    </div>
+
+    <div class="form-group">
+        <label class="col-md-4 control-label">Email</label>
+        <div class="col-md-6">
+            {{ Form::label('email', $agent->email, ['class' => 'form-control']) }}
+        </div>
+    </div>
+
+    <div class="form-group">
+        {{ Form::label('role', 'Role', ['class' => 'col-md-4 control-label']) }}
+        <div class="col-md-6">
+            {{ Form::label('role', $agent->role, ['class' => 'form-control']) }}
+        </div>
+    </div>
+
+    <div class="form-group">
+        {!! Form::label('department_id', 'Department ID', ['class' => 'col-md-4 control-label']) !!}
+        <div class="col-md-6">
+            {{ Form::label('department_id', $agent->department_id, ['class' => 'form-control']) }}
+        </div>
+    </div>
+
+    <div class="form-group">
+        {!! Form::label('gender', 'Gender', ['class' => 'col-md-4 control-label']) !!}
+        <div class="col-md-6">
+            {{ Form::label('gender', $agent->gender == 0 ? 'Not Defined' : ($agent->gender == 1 ? 'Female' : 'Male'), ['class' => 'form-control']) }}
+        </div>
+    </div>
+
+    <div class="form-group">
+        {!! Form::label('date_of_birth', 'Date Of Birth', ['class' => 'col-md-4 control-label']) !!}
+        <div class="col-md-6">
+            {{ Form::label('date_of_birth', $agent->date_of_birth, ['class' => 'form-control']) }}
+        </div>
+    </div>
+@endsection
+
 @section('content')
     <div class="margin-top container">
         <div class="row">
@@ -9,51 +53,7 @@
                     <div class="panel-body">
                         @if($agent->editable())
                             {{ Form::model($agent, array('class' => "form-horizontal", 'method' => 'POST', 'route' => array('agent.edit', $agent->id))) }}
-                        @endif
-                        
-                        <div class="form-group">
-                            <label class="col-md-4 control-label">Name</label>
-                            <div class="col-md-6">
-                                {{ Form::label('name', $agent->name, ['class' => 'form-control']) }}
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <label class="col-md-4 control-label">Email</label>
-                            <div class="col-md-6">
-                                {{ Form::label('email', $agent->email, ['class' => 'form-control']) }}
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            {{ Form::label('role', 'Role', ['class' => 'col-md-4 control-label']) }}
-                            <div class="col-md-6">
-                                {{ Form::label('role', $agent->role, ['class' => 'form-control']) }}
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            {!! Form::label('department_id', 'Department ID', ['class' => 'col-md-4 control-label']) !!}
-                            <div class="col-md-6">
-                                {{ Form::label('department_id', $agent->department_id, ['class' => 'form-control']) }}
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            {!! Form::label('gender', 'Gender', ['class' => 'col-md-4 control-label']) !!}
-                            <div class="col-md-6">
-                                {{ Form::label('gender', $agent->gender == 0 ? 'Not Defined' : ($agent->gender == 1 ? 'Female' : 'Male'), ['class' => 'form-control']) }}
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            {!! Form::label('date_of_birth', 'Date Of Birth', ['class' => 'col-md-4 control-label']) !!}
-                            <div class="col-md-6">
-                                {{ Form::label('date_of_birth', $agent->date_of_birth, ['class' => 'form-control']) }}
-                            </div>
-                        </div>
-
-                        @if($agent->editable())
+                            @yield('form_content')
                             <div class="form-group">
                                 <div class="col-md-6 col-md-offset-4">
                                     <button type="submit" class="btn btn-primary pull-right">
@@ -61,9 +61,12 @@
                                     </button>
                                 </div>
                             </div>
+                            {{ Form::close() }}
+                        @else
+                            <div class="form-horizontal">
+                                @yield('form_content')
+                            </div>
                         @endif
-
-                        {{ Form::close() }}
                     </div>
                 </div>
             </div>
