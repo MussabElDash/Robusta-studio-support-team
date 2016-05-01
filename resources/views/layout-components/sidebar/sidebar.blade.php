08d189c4782a5bd1b929334aceed2b732194de2a
Mussab ElDash <mussab14@gmail.com>
2016-05-02 00:31:09 +0200

    profile in sidebar show the current user and add the active class of it and the ticket pool

--- resources/views/layout-components/sidebar/sidebar.blade.php
+++ resources/views/layout-components/sidebar/sidebar.blade.php
@@ -24,13 +24,13 @@
             <li class="{{ active('departments.*') }}">
                 <a href="/departments"><i class="fa fa-dashboard"></i> <span>Departments</span></a>
             </li>
-            <li class="{{ active('agents.*') }}">
+            <li class="{{ active(['agents.*', 'not:agents/' . $user->slug]) }}">
                 <a href="/agents"><i class="fa fa-users"></i> <span>Agents</span></a>
             </li>
-            <li>
-                <a href="profile.html"><i class="fa fa-user"></i> <span>Profile</span></a>
+            <li class="{{ active('agents/' . $user->slug) }}">
+                <a href="/agents/{{$user->slug}}"><i class="fa fa-user"></i> <span>Profile</span></a>
             </li>
-            <li>
+            <li class="{{ active('tickets.pool') }}">
                 <a href="{{ route('tickets.pool') }}">
                     <i class="fa fa-sticky-note-o"></i>
                     <span>Tickets Pool</span>
