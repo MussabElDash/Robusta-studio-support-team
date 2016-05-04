fd6636d1a07c43b9efa368edba8875fd5eab0c54
mahmoudawadeen <mahmoud.awadeen@gmail.com>
2016-05-04 20:46:20 +0200

    Merge branch 'develop' of https://github.com/MussabElDash/Robusta-studio-support-team into feature/change_twitter_settings
    # Conflicts:
    #	app/Http/Controllers/AgentsController.php

---
+++ resources/views/tickets/paypal.blade.php
@@ -0,0 +1,52 @@
+@extends('layouts.landing')
+
+@section('content')
+    <div class="margin-top container paypal">
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+                <div class="panel panel-default">
+                    <div class="panel-heading"><i class="fa fa-paypal"></i>remium
+                        subscription<img src="/assets/images/200.png"></div>
+                    <div class="panel-body">
+                        {!! Form::open(['route' =>  'tickets.vip','method' => 'post', 'class' => 'form-horizontal']) !!}
+                        {!! Form::hidden('ticket',$ticket,array('id' => 'customer_twitter_id')) !!}
+                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
+                            <label class="col-md-4 control-label">E-Mail Address</label>
+
+                            <div class="col-md-6">
+                                <input type="email" class="form-control" name="email" value="{{ old('email') }}">
+
+                                @if ($errors->has('email'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('email') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
+                            <label class="col-md-4 control-label">Password</label>
+
+                            <div class="col-md-6">
+                                <input type="password" class="form-control" name="password">
+
+                                @if ($errors->has('password'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('password') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <div class="col-md-6 col-md-offset-4">
+                                <p class="total">Total of 20$</p>
+                                {!! Form::submit('Pay', ['class' => 'btn btn-success pull-right']) !!}
+                            </div>
+                        </div>
+                        {!! Form::close() !!}
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
